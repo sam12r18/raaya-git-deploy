e7784f1333b2fc3fe3c4aff9bf266500e63949d1
سروش اربابی <sam12r18@live.com>
2026-09-14 22:47:22 +0330

    build: add initial WinUI main window code-behind

---
+++ src/RaayaGitDeploy.App/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace RaayaGitDeploy.App;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
