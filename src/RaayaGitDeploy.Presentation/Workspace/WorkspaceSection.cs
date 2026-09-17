9690baf0dfb8803a1b97e949721b5943b27bb5bc
سروش اربابی <sam12r18@live.com>
2026-09-17 11:50:03 +0330

    feat: add workbench workspace sections

---
+++ src/RaayaGitDeploy.Presentation/Workspace/WorkspaceSection.cs
@@ -0,0 +1,12 @@
+namespace RaayaGitDeploy.Presentation.Workspace;
+
+public enum WorkspaceSection
+{
+    Changes,
+    Commits,
+    Terminal,
+    Commands,
+    DeployQueue,
+    Servers,
+    History
+}
